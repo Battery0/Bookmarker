7a85ba51ec69ee5d7d7a9b5516baa66a89b1887a
ian k <kickthechair@gmail.com>
2021-08-19 18:05:09 +0100

    step 12 complete - DB protected against SQL injection attacks

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -30,7 +30,15 @@ class Bookmarks
       con = PG.connect :dbname => 'bookmark_manager'
     end
     
-    rs = con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title"
-    Bookmarks.new(id: rs[0]['id'], title: rs[0]['title'], url: rs[0]['url'])
+    # rs = con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title"
+    
+    result = con.exec_params(
+      # The first argument is our SQL query template
+      # The second argument is the 'params' referred to in exec_params
+      # $1 refers to the first item in the params array
+      # $2 refers to the second item in the params array
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
+    Bookmarks.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
